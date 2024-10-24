ef3252c32b83c74cd567375d75096e07233e75fd
Mili Gularte <miligularte182@gmail.com>
2024-10-24 00:50:05 -0300

    primer commit

---
+++ slider.js
@@ -0,0 +1,15 @@
+const slides = document.querySelectorAll('.proyectos-slider .slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Cambia de imagen cada 3 segundos
